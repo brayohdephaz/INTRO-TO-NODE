5fef15837f2ef86d5be0c60b24ae79b19159512e
Brian Murithi <bm.mutwiri@gmail.com>
2019-12-27 00:45:29 -0700

    initial commit

---
+++ hello.js
@@ -0,0 +1,38 @@
+//console.log("Hello world!");
+var http = require('http');
+// var url = require('url');
+
+// http.createServer(function (req, res){
+// 	res.writeHead(200,{'content-type': 'text/html'});
+// 	var q = url.parse(req.url, true).query;
+// 	var dates = q.year + " " + q.month;
+// 	res.end(dates);
+// }).listen(8080);
+
+
+//CALLING THE FILE SYSTEM MODULE
+ var fs = require('fs');
+ var url = require('url');
+
+ http.createServer(function (req, res){
+ 	var q = url.parse(req.url , true);
+ 	var filename = "." + q.pathname;//Helps run and navigate over files and pages
+ 	if(filename=='./'){
+ 		filename='./index';//helps display index file if no pah is passed
+ 	}
+ 	filename = filename + ".html";//remove the html extension on the filename
+ 	fs.readFile(filename, function(err, data){
+ 		if(err){
+ 			res.writeHead(404, {'content-type':'text/html'});//erro message incase file not found
+ 			return res.end("ERROR: 404 Found!");//returns block if error occurs.
+ 		}
+ 		res.writeHead(200, {'content-type':'text/html'});
+ 		res.write(data);
+ 		return res.end();
+ 	});
+ }).listen(8080);
+
+ console.log("Server listening to port 8080....");
+
+
+ //
